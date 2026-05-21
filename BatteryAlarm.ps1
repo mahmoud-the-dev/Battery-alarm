21c2c4e2d604f45373f0139db66c608de6cef7b0
MA <edu.mahmoud.ahmed@gmail.com>
2026-05-21 23:16:53 +0300

    Add configurable alarm snooze behavior

--- BatteryAlarm.ps1
+++ BatteryAlarm.ps1
@@ -14,6 +14,7 @@ $DefaultConfig = [ordered]@{
     NotificationIntervalSeconds = 60
     AlarmSoundDurationSeconds = 12
     AlarmSoundVolumePercent = 100
+    SnoozeMinutes = 5
 }
 
 function Save-DefaultConfig {
@@ -42,6 +43,7 @@ function Get-AppConfig {
     $notify = [int]$config.NotificationIntervalSeconds
     $soundDuration = [int]$config.AlarmSoundDurationSeconds
     $soundVolume = [int]$config.AlarmSoundVolumePercent
+    $snooze = [int]$config.SnoozeMinutes
 
     if ($low -lt 1 -or $low -gt 99) { $low = $DefaultConfig.LowBatteryLimit }
     if ($high -lt 1 -or $high -gt 100) { $high = $DefaultConfig.HighBatteryLimit }
@@ -49,6 +51,7 @@ function Get-AppConfig {
     if ($notify -lt 10) { $notify = $DefaultConfig.NotificationIntervalSeconds }
     if ($soundDuration -lt 1 -or $soundDuration -gt 60) { $soundDuration = $DefaultConfig.AlarmSoundDurationSeconds }
     if ($soundVolume -lt 1 -or $soundVolume -gt 100) { $soundVolume = $DefaultConfig.AlarmSoundVolumePercent }
+    if ($snooze -lt 1 -or $snooze -gt 1440) { $snooze = $DefaultConfig.SnoozeMinutes }
 
     [pscustomobject]@{
         LowBatteryLimit = $low
@@ -57,6 +60,7 @@ function Get-AppConfig {
         NotificationIntervalSeconds = $notify
         AlarmSoundDurationSeconds = $soundDuration
         AlarmSoundVolumePercent = $soundVolume
+        SnoozeMinutes = $snooze
     }
 }
 
@@ -119,10 +123,7 @@ function Play-AlarmSound {
     try {
         $soundPath = New-AlarmSoundFile $Config.AlarmSoundDurationSeconds $Config.AlarmSoundVolumePercent
 
-        if ($script:AlarmPlayer -ne $null) {
-            $script:AlarmPlayer.Stop()
-            $script:AlarmPlayer.Dispose()
-        }
+        Stop-AlarmSound
 
         $script:AlarmPlayer = New-Object System.Media.SoundPlayer($soundPath)
         $script:AlarmPlayer.Load()
@@ -130,6 +131,14 @@ function Play-AlarmSound {
     }
     catch {
         [System.Media.SystemSounds]::Exclamation.Play()
+    }
+}
+
+function Stop-AlarmSound {
+    if ($script:AlarmPlayer -ne $null) {
+        $script:AlarmPlayer.Stop()
+        $script:AlarmPlayer.Dispose()
+        $script:AlarmPlayer = $null
     }
 }
 
@@ -147,6 +156,7 @@ $Config = Get-AppConfig
 $LastNotificationAt = [DateTime]::MinValue
 $LastAlarmKind = ''
 $AlarmPlayer = $null
+$SnoozedUntil = [DateTime]::MinValue
 
 $Context = New-Object System.Windows.Forms.ApplicationContext
 $Icon = New-Object System.Windows.Forms.NotifyIcon
@@ -175,6 +185,11 @@ function Show-Notice($title, $message) {
     $Icon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
     $Icon.ShowBalloonTip(10000)
     Play-AlarmSound
+}
+
+function Snooze-Alarm {
+    $script:SnoozedUntil = (Get-Date).AddMinutes($Config.SnoozeMinutes)
+    Stop-AlarmSound
 }
 
 function Update-StatusText($snapshot) {
@@ -207,12 +222,19 @@ function Test-Battery {
         }
 
         if ($alarmKind -eq '') {
+            Stop-AlarmSound
+            $script:SnoozedUntil = [DateTime]::MinValue
             $script:LastAlarmKind = ''
             $script:LastNotificationAt = [DateTime]::MinValue
             return
         }
 
         $now = Get-Date
+        if ($script:SnoozedUntil -gt $now) {
+            return
+        }
+
+        $script:SnoozedUntil = [DateTime]::MinValue
         $secondsSinceLastNotice = ($now - $script:LastNotificationAt).TotalSeconds
         if ($alarmKind -ne $script:LastAlarmKind -or $secondsSinceLastNotice -ge $Config.NotificationIntervalSeconds) {
             Show-Notice $title $message
@@ -232,11 +254,7 @@ $ReloadItem.Add_Click({
 })
 
 $ExitItem.Add_Click({
-    if ($script:AlarmPlayer -ne $null) {
-        $script:AlarmPlayer.Stop()
-        $script:AlarmPlayer.Dispose()
-    }
-
+    Stop-AlarmSound
     $Icon.Visible = $false
     $Icon.Dispose()
     $Context.ExitThread()
@@ -246,6 +264,17 @@ $Timer = New-Object System.Windows.Forms.Timer
 $Timer.Interval = [Math]::Max(5, $Config.CheckIntervalSeconds) * 1000
 $Timer.Add_Tick({ Test-Battery })
 $Timer.Start()
+
+$PowerModeChangedHandler = {
+    Test-Battery
+}
+[Microsoft.Win32.SystemEvents]::add_PowerModeChanged($PowerModeChangedHandler)
+$Icon.Add_BalloonTipClosed({ Snooze-Alarm })
+
+$Context.add_ThreadExit({
+    [Microsoft.Win32.SystemEvents]::remove_PowerModeChanged($PowerModeChangedHandler)
+    Stop-AlarmSound
+})
 
 Test-Battery
 [System.Windows.Forms.Application]::Run($Context)
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ A small Windows background tray app that reminds you to plug or unplug the charg
 
 - If the charger is unplugged and battery is at or below `LowBatteryLimit`, it sends a notification and plays a loud alarm sound every minute until the charger is plugged in.
 - If the charger is plugged in and battery is at or above `HighBatteryLimit`, it sends a notification and plays a loud alarm sound every minute until the charger is unplugged.
+- Closing a notification stops the alarm sound and snoozes battery alerts for `SnoozeMinutes`.
 - The app runs in the Windows notification area. Right-click the tray icon to reload config or exit.
 - The alarm sound is generated automatically as a WAV file in the app folder the first time it is needed.
 
@@ -26,13 +27,15 @@ Edit `config.json`:
   "CheckIntervalSeconds": 30,
   "NotificationIntervalSeconds": 60,
   "AlarmSoundDurationSeconds": 12,
-  "AlarmSoundVolumePercent": 100
+  "AlarmSoundVolumePercent": 100,
+  "SnoozeMinutes": 5
 }
 ```
 
 Right-click the tray icon and choose `Reload config` after changing the file.
 
 `AlarmSoundDurationSeconds` can be set from `1` to `60`. `AlarmSoundVolumePercent` can be set from `1` to `100`.
+`SnoozeMinutes` can be set from `1` to `1440`.
 
 ## Start With Windows
 
